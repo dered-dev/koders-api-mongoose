3d3ce12e6f20d588b7582747d6c17bde7d578776
jorge luis camarillo <jlcc170@gmail.com>
2020-02-10 17:10:06 -0600

    Complete CRUD koders

--- src/routes/koders.js
+++ src/routes/koders.js
@@ -22,5 +22,92 @@ router.get('/', async (request, response) => {
     })
   }
 })
+router.post('/create', async (request, response) => {
+  try {
+    var {
+      name,
+      gender,
+      email,
+      birthDate
+    } = request.query
+    const newKoder = await koder.create({ name, gender, email, birthDate })
+    response.json({
+      success: true,
+      message: 'New koder',
+      data: {
+        newKoder
+      }
+    })
+  } catch (error) {
+    response.status(400)
+    response.json({
+      success: false,
+      message: error.message
+    })
+  }
+})
+router.post('/update/:id', async (request, response) => {
+  try {
+    var idKoder = request.params.id
+    var {
+      name,
+      gender,
+      email,
+      birthDate
+    } = request.query
+    const koderUpdated = await koder.updateById(idKoder, { name, gender, email, birthDate })
+    response.json({
+      success: true,
+      message: 'koder Updated',
+      data: {
+        koderUpdated
+      }
+    })
+  } catch (error) {
+    response.status(400)
+    response.json({
+      success: false,
+      message: error.message
+    })
+  }
+})
+router.get('/getById/:id', async (request, response) => {
+  try {
+    var { id } = request.params
+    const findedKoder = await koder.getById(id)
+    response.json({
+      success: true,
+      message: ' koder Data',
+      data: {
+        findedKoder
+      }
+    })
+  } catch (error) {
+    response.status(400)
+    response.json({
+      success: false,
+      message: error.message
+    })
+  }
+})
+router.delete('/deleteById/:id', async (request, response) => {
+  try {
+    var { id } = request.params
+    const koderDeleted = await koder.deleteByid(id)
+    response.json({
+      success: true,
+      message: 'koder Deleted',
+      data: {
+        koderDeleted
+      }
+    })
+  } catch (error) {
+    response.status(400)
+    response.json({
+      success: false,
+      message: error.message
+    })
+  }
+})
 
 module.exports = router
--- src/usecases/koder.js
+++ src/usecases/koder.js
@@ -1,21 +1,28 @@
 
 const koder = require('../models/koder')
 
+// Ok
+// test on localhost:8080/koders/create?name=jorge luis&gender=m&email=test@update.com&birthDate=1991-05-12
 function create ({ name, gender, email, birthDate }) {
   const newKoder = new koder({ name, gender, email, birthDate })
   return newKoder.save() // is a promise
 }
+// ok
+// test on localhost:8080/koders/deleteById/5e41da9a8b235270737fcf28
 function deleteByid (id) {
-  // const koderFound = await koder.findById(id)
-  // return koderFound.remove()
   return koder.findByIdAndDelete(id)
 }
+// ok localhost:8080/koders
 function getAll () {
   return koder.find({})
 }
+// ok
+// test on localhost:8080/koders/getById/5e41da9a8b235270737fcf28
 function getById (id) {
   return koder.findById(id)
 }
+// ok
+// test on localhost:8080/koders/update/5e41da9a8b235270737fcf28?name=jorge luis&gender=m&email=test@update.com&birthDate=1991-05-12
 function updateById (id, infoToUpdate) {
   return koder.findByIdAndUpdate(id, infoToUpdate)
 }
